56ee48cbbf0b7aa540382a1b506dc0a8613afb34
SnorlaxSPI <aflinsjr@gmail.com>
2021-08-20 12:10:45 -0300

    integração node - html

--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (request, response) => {
-  response.sendFile(__dirname + '/html/index.html')
-  response.sendFile(__dirname + '/css')
-  response.sendFile(__dirname + '/assets')
+  response.sendFile(__dirname + '/html/index.html')  
 })
 
 export { router };
